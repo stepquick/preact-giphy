5b52c1ad0a86d270f13d456675d4aad80f346833
Stephen Quick <quick.stephen@gmail.com>
2017-01-29 01:06:12 -0500

    fix resize on the flexbox

--- src/app/components/Result.js
+++ src/app/components/Result.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
-const style = {
-  width: '100%',
+const imgStyle = {
+    width: '100%',
+    height: 'auto'
+};
+
+const divStyle = {
+    maxWidth: '100%'
 };
 
 const Result = React.createClass({
     render() {
         return (
-            <div className="column column-33">
-                <img src={this.props.url} style={style}/>
+            <div className="column column-33" style={divStyle}>
+                <img src={this.props.url} style={imgStyle}/>
             </div>
         );
     }
