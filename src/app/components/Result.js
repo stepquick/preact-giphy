674efed01d359c52cb4434c2f595c84ecc9e899e
Stephen Quick <quick.stephen@gmail.com>
2017-01-29 00:52:43 -0500

    full width the image

--- src/app/components/Result.js
+++ src/app/components/Result.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+const style = {
+  width: '100%',
+};
+
 const Result = React.createClass({
     render() {
         return (
             <div className="column column-33">
-                <img src={this.props.url}/>
+                <img src={this.props.url} style={style}/>
             </div>
         );
     }
